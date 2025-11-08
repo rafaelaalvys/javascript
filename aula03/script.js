92d25a5505c28c9194bea84f7963f2cf71e2c7b3
rafaelaalvys <rafaela.alvys2020@outlook.com>
2025-11-07 21:42:38 -0300

    aula-sexta

---
+++ aula03/script.js
@@ -0,0 +1,8 @@
+// Veja abaixo exemplo de como e quando usar a crases
+
+var nome = 'rafaela';
+var idade = 25; //Número usados sem aspas (simples ou duplas),
+
+var mensagem = `Olá, meu nome é ${nome} e tenho ${idade} anos`;
+
+conseole.log(mensagem); //Utilizado para fazer a impressão do resultado na tela
